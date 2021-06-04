5bd27bdf8211a657cb68560dd9529085adff735a
Samuel Huff <samueljhuff@gmail.com>
2021-06-03 22:16:04 -0700

    Fix email template and add manifest queries

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,3 +1,3 @@
 const kSubmitBugUrl = 'https://github.com/huffSamuel/upnp_explorer_issues/issues/new/choose';
 const kSubmitBugEmail = 'samueljhuff+upnp@gmail.com';
-const kSubmitBugQuery = 'subject=%s&body=%s %s';
+const kSubmitBugQuery = 'subject=%s&body=%s';
