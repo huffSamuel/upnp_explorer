09c72da6a98a18802a73b35265fe8a2779ff3ba8
Samuel Huff <samueljhuff@gmail.com>
2022-11-03 23:34:18 -0700

    Undo rename, update build properties

--- lib/application/application.dart
+++ lib/application/application.dart
@@ -8,7 +8,7 @@ class Application {
   static FluroRouter? router;
   static const submitBugUrl =
       'https://github.com/huffSamuel/upnp_explorer_issues/issues/new/choose';
-  static const name = 'UPnP Scanner';
+  static const name = 'UPnP Explorer';
   static const privacyPolicyUrl =
       'https://github.com/huffSamuel/upnp_explorer_issues/blob/main/PRIVACY_POLICY.md';
   static const appId = 'com.samueljhuf.upnp_explorer';
