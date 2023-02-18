ad4fdca9c1a68e5477900c8d697cb81d50909a54
Samuel Huff <samueljhuff@gmail.com>
2023-02-18 00:22:51 -0800

    refactor: remove unused references

--- lib/presentation/changelog/page/changelog_page.dart
+++ lib/presentation/changelog/page/changelog_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 
 import '../../../application/changelog/changelog_service.dart';
 import '../../../application/ioc.dart';
-import '../../../domain/changelog/change_version.dart';
 import '../../core/page/app_page.dart';
 
 class ChangelogPage extends StatefulWidget {
@@ -43,58 +42,6 @@ class _ChangelogPageState extends State<ChangelogPage> {
       sliver: _loading
           ? null
           : SliverFillRemaining(child: Markdown(data: _changes)),
-    );
-  }
-}
-
-class _ChangelogVersion extends StatelessWidget {
-  final bool current;
-  final ChangeVersion change;
-
-  const _ChangelogVersion({
-    Key? key,
-    required this.change,
-    this.current = false,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-
-    var titleTheme = textTheme.titleLarge!;
-    var bodyTheme = textTheme.bodyMedium!;
-
-    if (current) {
-      titleTheme = titleTheme.copyWith(fontWeight: FontWeight.w500);
-
-      bodyTheme = bodyTheme.copyWith(
-        fontWeight: FontWeight.w500,
-      );
-    }
-
-    return Padding(
-      padding: const EdgeInsets.only(
-        left: 24.0,
-        right: 8.0,
-        bottom: 16.0,
-        top: 8.0,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(change.version, style: titleTheme),
-          const SizedBox(height: 4.0),
-          ...change.changes.map(
-            (x) => Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: Text(
-                AppLocalizations.of(context)!.changelogItem(x),
-                style: bodyTheme,
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
