a0842840b032ae0cad6459a00232579e079e9322
Samuel Huff <samueljhuff@gmail.com>
2024-06-13 10:27:00 -0700

    refactor: clean up startup file logic (#109)

---
+++ lib/startup.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+import 'application/flavors/flavor_features.dart';
+import 'application/ioc.dart';
+import 'application/licenses.dart';
+import 'application/settings/options_repository.dart';
+import 'presentation/core/widgets/model_binding.dart';
+import 'presentation/device/pages/service.dart';
+import 'upnp_explorer.dart';
+
+Future<void> runAppWithFeatures(FlavorFeatures features) async {
+  registerLicenses();
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await configureDependencies();
+
+  GetIt.instance.registerSingleton<FlavorFeatures>(features);
+  
+  runApp(
+    ModelBinding(
+      initialModel: sl<SettingsRepository>().get(),
+      onUpdate: (c, m) {
+        sl<SettingsRepository>().set(m).then((_) async {
+          if (c?.protocolOptions == m.protocolOptions) {
+            return;
+          }
+
+          final svc = sl<DiscoveryStateService>();
+          await svc.update(m.protocolOptions);
+        });
+      },
+      child: UPnPExplorer(
+        optionsRepository: sl(),
+      ),
+    ),
+  );
+}
