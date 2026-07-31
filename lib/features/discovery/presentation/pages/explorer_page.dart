27112771c037cf3f8701f9d8eb5f09e38967016c
Samuel Huff <samueljhuff@gmail.com>
2026-07-31 10:00:34 -0700

    feature: remove changelog dialog at startup

--- lib/features/discovery/presentation/pages/explorer_page.dart
+++ lib/features/discovery/presentation/pages/explorer_page.dart
@@ -1,46 +1,19 @@
 import 'package:flutter/material.dart';
 
-import '../../../../application/changelog/changelog_service.dart';
 import '../../../../application/ioc.dart';
-import '../../../../application/routing/routes.dart';
 import '../../../../extension/build_context.dart';
-import '../../../core/presentation/pages/changelog_page.dart';
 import '../../../core/presentation/widgets/page_title.dart';
 import '../../logic/service.dart';
 import '../widgets/loaded_list.dart';
 import '../widgets/refresh_button.dart';
 
-class ExplorerPage extends StatefulWidget {
+class ExplorerPage extends StatelessWidget {
   final DiscoveryStateService _service;
-  final ChangelogService _changelog;
 
   ExplorerPage({
     super.key,
     DiscoveryStateService? service,
-    ChangelogService? changelog,
-  })  : _service = service ?? sl<DiscoveryStateService>(),
-        _changelog = changelog ?? sl<ChangelogService>();
-
-  @override
-  State<ExplorerPage> createState() => _ExplorerPageState();
-}
-
-class _ExplorerPageState extends State<ExplorerPage> {
-  @override
-  void initState() {
-    // TODO: Handle this as a dialog when the app starts
-    widget._changelog.shouldDisplayChangelog().then((display) {
-      if (display) {
-        Navigator.of(context).push(
-          makeRoute(
-            context,
-            ChangelogPage(),
-          ),
-        );
-      }
-    });
-    super.initState();
-  }
+  }) : _service = service ?? sl<DiscoveryStateService>();
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +21,7 @@ class _ExplorerPageState extends State<ExplorerPage> {
 
     final actions = [
       RefreshIconButton(
-        service: widget._service,
+        service: _service,
       ),
     ];
 
@@ -68,7 +41,7 @@ class _ExplorerPageState extends State<ExplorerPage> {
       ),
       body: Center(
         child: StreamBuilder(
-          stream: widget._service.state,
+          stream: _service.state,
           builder: (context, snapshot) {
             if (!snapshot.hasData ||
                 snapshot.hasError ||
@@ -77,7 +50,7 @@ class _ExplorerPageState extends State<ExplorerPage> {
             }
 
             return LoadedList(
-              onRefresh: widget._service.search,
+              onRefresh: _service.search,
               scanning: snapshot.data!.scanning,
               devices: snapshot.data!.devices,
               noNetwork: !snapshot.data!.viableNetwork,
